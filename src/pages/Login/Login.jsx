cac19a3bb6bb303eb93334d454e9d886d3d12ef9
unknown <fahadkhalife70@gmail.com>
2025-07-16 16:09:39 +0530

    resolve login built error

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './login.scss'
+import './Login.scss'
 import logo from '../../assets/logo.png'
 import { login,signup } from '../../firebase'
 import netflix_spinner from '../../assets/netflix_spinner.gif'
